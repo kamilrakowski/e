76fe6715d57ab241b39e6c7a542b31ed913637be
Flashwave <julianvdg@gmail.com>
2014-11-09 00:52:59 +0100

    Please die Apple.

--- e.m
+++ e.m
@@ -1,1 +1,11 @@
-printf('e');
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+        NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+        NSLog (@"e");
+        [pool drain];
+        return 0;
+}
+
+// THIS SHIT IS FUCKING HORRIBLE
